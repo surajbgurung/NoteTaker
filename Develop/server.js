0bb72019b4cdcf9d1a730bfa9730a3575d6e1fc1
surajgurung <microbiosuraj@gmail.com>
2020-05-18 14:05:38 -0400

    commenting all the method and giving final touch

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,13 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-// var noteId=0;
+
 
 //set up the express and port
 var app = express();
 var PORT = 3000;
+
+//this is use for static file
 app.use(express.static('public'))
 
 
@@ -22,10 +24,12 @@ app.get("/notes", function (req, res) {
     //console.log(__dirname)
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
+
 app.get("/api/notes", function (req, res) {
     //res.json(newNote);
     res.sendFile(path.join(__dirname, "./db/db.json"))
 });
+
 //display a single character,or return false
 app.get("/api/notes/:id", function (req, res) {
     //var choosen = req.params;
@@ -35,39 +39,30 @@ app.get("/api/notes/:id", function (req, res) {
     // console.log("index", Number(req.params.id));//it gives index number of object array;
 
     //console.log("choosen....", myid);
-    
+
 });
 
 
 
 
-//post route
+//post route,what ever user post 
 app.post("/api/notes", function (req, res) {
-    //read the file in db.json file and assign to the variable
-    // var readFile = fs.readFile("./db/db.json", "utf8", function (error, data) {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    // });
-    //parsing the readfile and making it object and assign to variable
+    //at first our given file db.json is read and  stored in noteData
     var noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    // var noteData=JSON.parse(readFile);
+  //request from the user is assign to the newNote
     var newNote = req.body;
-    var uniqueID = (noteData.length).toString();
-    newNote.id = uniqueID;
+    //to create id now comparing its id with its length and make it to string
+    var uniqueId = (noteData.length).toString();
+    //new id is created by .id and assigning to uniqueID
+    newNote.id = uniqueId;
     console.log("new data enter", newNote);
 
-
+//so what ever data comes from the user, so it is push to the noteData object
     noteData.push(newNote);
     console.log("inside the array", noteData);
-    //after pushing new data into object and writing that object into our file db.json
-    // fs.writeFile("./db/db.json", JSON.stringify(noteData), function (err) {
-    //     if (err) {
-    //         throw err;
-    //     }
-    // });
+    //the the noteData is writtten by syn method
     fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
-
+//then the file is send to its respective path;
     res.sendFile(path.join(__dirname, "./db/db.json"))
 
 });
@@ -76,37 +71,31 @@ app.post("/api/notes", function (req, res) {
 //route for delete
 app.delete("/api/notes/:id", function (req, res) {
     console.log("Called")
-    // var readFile = fs.readFile("./db/db.json", "utf8", function (error, data) {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    // });
-   // var noteData=JSON.parse(readFile);
-    var noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
+    var noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+//what the user enter is captured in choosen variable
     var choosen = req.params.id;
     console.log(choosen)
-    /// noteData.splice(Number(req.params.id),1);
-    var newId = 0;
-    noteData = noteData.filter(currentNote => {
-        return currentNote.id != choosen;
-    });
-    for (currentNote of noteData) {
-        currentNote.id = newId.toString();
-        newId++;
 
-        // fs.writeFile("./db/db.json", JSON.stringify(noteData), function (err) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        // });
+    var newId = 0;
+    //if noteData length is more than zero only we save the file. here filter method is used to save the data(file) except choosen
+    if (noteData.length > 0) {
+        noteData = noteData.filter(currentNote => {
+            return currentNote.id != choosen;
+        });
+        for (currentNote of noteData) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        //again write the file of noteData
         fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
-        //res.sendFile(path.join(__dirname, "./db/db.json"));
-        res.json(noteData);
-    }
+        res.sendFile(path.join(__dirname, "./db/db.json"));
+    }else return;
+    
+});
 
    
-});
+
 
 //route for home page...
 app.get("*", function (req, res) {
