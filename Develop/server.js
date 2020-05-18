0abae550473967cd4e0f3a047e2fe173e9b59025
surajgurung <microbiosuraj@gmail.com>
2020-05-18 14:35:08 -0400

    new file

--- Develop/server.js
+++ Develop/server.js
@@ -98,7 +98,7 @@ app.delete("/api/notes/:id", function (req, res) {
 
 
 //route for home page...
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
